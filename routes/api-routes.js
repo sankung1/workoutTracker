c231e6a23e5f5831ba2a29527daee4ea308ad9cc
Sankung Sillah <ssillah306@gmail.com>
2020-12-13 21:46:36 -0500

    quick fix

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -23,12 +23,12 @@ module.exports = (app) => {
       .then((data) => res.json(data))
       .catch((err) => res.status(400).json(err));
   });
-};
 
-app.put("/api/workouts/:id", (req, res) => {
-  db.Workout.findByIdAndUpdate(req.params.id, {
-    $push: { exercises: req.body },
-  })
-    .then((data) => res.json(data))
-    .catch((err) => res.status(400).json(err));
-});
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, {
+      $push: { exercises: req.body },
+    })
+      .then((data) => res.json(data))
+      .catch((err) => res.status(400).json(err));
+  });
+};
