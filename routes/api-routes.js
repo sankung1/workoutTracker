c973511ce09f6b23a8cb34c173db811c9c470032
Sankung Sillah <ssillah306@gmail.com>
2020-12-13 21:46:06 -0500

    added the needed api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,16 +1,34 @@
 const db = require("../models");
 module.exports = (app) => {
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then((data) => res.json(data))
+      .catch((err) => res.status(400).json(err));
+  });
 
-    app.get("/api/workouts", (req, res) => {
-        db.Workout.find({})
-            .then(data => res.json(data))
-            .catch(err => res.json(err));
-    });
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
+      .then((data) => res.json(data))
+      .catch((err) => res.status(400).json(err));
+  });
 
-    // getting the created workout 
-    app.get("/api/workouts/range", (req, res) => {
-        db.Workout.create({})
-            .then(data => res.json(data))
-            .catch(err => res.json(err));
-    });
-}
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find()
+      .then((data) => res.json(data))
+      .catch((err) => res.status(400).json(err));
+  });
+
+  app.post("/api/workouts/range", function (req, res) {
+    db.Workout.create({})
+      .then((data) => res.json(data))
+      .catch((err) => res.status(400).json(err));
+  });
+};
+
+app.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findByIdAndUpdate(req.params.id, {
+    $push: { exercises: req.body },
+  })
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json(err));
+});
