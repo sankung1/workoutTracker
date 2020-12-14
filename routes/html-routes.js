8b342070e1c3989417e071188b25cc7a15dd0c44
Sankung Sillah <ssillah306@gmail.com>
2020-12-13 21:07:52 -0500

    added a get route send the user to the index and stats page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,8 +2,18 @@
 const path = require("path");
 
 module.exports = (app) =>{
+
+    //get route to send the user to the home page
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
     // get route to send the user to the exercise page
     app.get("/exercise", (req, res)=>{
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+
+    // get route to send the user to stats page
+    app.get("/stats", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
 }
