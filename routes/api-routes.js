4d363db842911aae76c53b672ab288e0d9e50c5d
Sankung Sillah <ssillah306@gmail.com>
2020-12-13 21:28:08 -0500

    added a get api call to get the last workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,13 @@
-// requiring the models db
 const db = require("../models");
 module.exports = (app) =>{
 
-    //
+    app.get("/api/workouts", (req,res) =>{
+        db.Workout.find({})
+        .then(data =>{
+            res.json(data);
+        })
+        .catch(err =>{
+            res.json(err);
+        });
+    });
 }
