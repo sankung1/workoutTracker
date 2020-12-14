b27cd2aadc94f2f640ef4925b806ee5cfc56e1b9
Sankung Sillah <ssillah306@gmail.com>
2020-12-13 22:04:18 -0500

    created my express server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+
+
+app.listen(PORT, ()=>{
+    console.log("You are listening on " + PORT);
+})
